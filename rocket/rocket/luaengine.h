1add6500cd782b5b8864a052c39501d050743be9
king09fish <412540720@qq.com>
2015-01-15 09:04:30 +0800

    lua lib
    lua lib

---
+++ rocket/rocket/luaengine.h
@@ -0,0 +1,12 @@
+
+#ifndef _ROCKET_LUA
+#define _ROCKET_LUA
+
+extern "C"
+{
+#include <lualib/lua.h>
+#include <lualib/lualib.h>
+#include <lualib/lauxlib.h>
+}
+
+#endif
--- rocket/rocket/main.cpp
+++ rocket/rocket/main.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+#include"luaengine.h"
+
 
 int main(int argc, char *argv[])
 {
 
 	printf("hello world");
 	return 0;
+
+
 }
